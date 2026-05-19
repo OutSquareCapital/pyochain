6b3db59f39809fec59339fcab077e6f519606358
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-05-19 22:07:41 +0200

    feat: added benchmarks for `PyoIterator::unique`

---
+++ tests/benchmarks/test_unique.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+from typing import TYPE_CHECKING
+
+import pytest
+
+from pyochain import Range, Seq
+
+if TYPE_CHECKING:
+    from ._utils import BenchFixture
+SIZE = 4096
+
+
+def _run[T](data: Seq[T]) -> T:
+    return data.iter().unique().last()
+
+
+def _run_by[T, U](data: Seq[T], key: Callable[[T], U]) -> T:
+    return data.iter().unique_by(key).last()
+
+
+@pytest.mark.benchmark(group="unique")
+def test_int(benchmark: BenchFixture) -> None:
+    def _fn(x: int) -> int:
+        return 0 if x == SIZE - 1 else x
+
+    data = Range(0, SIZE).iter().map(_fn).collect()
+    assert benchmark(_run, data) == SIZE - 2
+
+
+@pytest.mark.benchmark(group="unique")
+def test_str(benchmark: BenchFixture) -> None:
+    def _fn(i: int) -> str:
+        return "0" if i == SIZE - 1 else str(i)
+
+    data = Range(0, SIZE).iter().map(_fn).collect()
+    assert benchmark(_run, data) == str(SIZE - 2)
+
+
+@pytest.mark.benchmark(group="unique_by")
+def test_int_by(benchmark: BenchFixture) -> None:
+    def _fn(x: int) -> int:
+        return 0 if x == SIZE - 1 else x
+
+    data = Range(0, SIZE).iter().map(_fn).collect()
+    assert benchmark(_run_by, data, lambda x: x + 10 - 10) == SIZE - 2
+
+
+@pytest.mark.benchmark(group="unique_by")
+def test_str_by(benchmark: BenchFixture) -> None:
+    def _fn(i: int) -> str:
+        return "0" if i == SIZE - 1 else str(i)
+
+    data = Range(0, SIZE).iter().map(_fn).collect()
+    assert benchmark(_run_by, data, str.lower) == str(SIZE - 2)
