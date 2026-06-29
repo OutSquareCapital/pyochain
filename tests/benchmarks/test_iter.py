7bc6048175af5f12614329139aff0376486c8537
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-06-29 03:03:57 +0200

    feat: new tests

--- tests/benchmarks/test_iter.py
+++ tests/benchmarks/test_iter.py
@@ -16,6 +16,18 @@ if TYPE_CHECKING:
     from ._utils import BenchFixture
 
 
+@pytest.mark.parametrize("size", SIZES)
+def test_iter(benchmark: BenchFixture, size: int) -> None:
+    data = Range(0, size)
+    assert benchmark(_iter, data) == 1
+
+
+def _iter(data: Range) -> int:
+    for _ in data:
+        _ = data.iter()
+    return 1
+
+
 @pytest.mark.benchmark(group="filter_map")
 @pytest.mark.parametrize("size", [64, 256, 1024, 4096])
 def test_filter_map(benchmark: BenchFixture, size: int) -> None:
