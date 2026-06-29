49d61ab3f0f7b1dd3a2c737c7812685b14600cf1
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-06-29 16:07:20 +0200

    feat: added typing tests

---
+++ tests/test_typing.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from pyochain import Iter
+
+if TYPE_CHECKING:
+    from pyochain.abc import PyoIterable, PyoIterator
+
+
+class Animal: ...
+
+
+class Dog(Animal): ...
+
+
+def check_covariance() -> None:
+    """This can't be introspected at runtime. The type checker step is what will catch errors."""
+    base: PyoIterable[Dog] = Iter(())
+    _abc_iterable: PyoIterable[Animal] = base
+    _abc_iterator: PyoIterator[Animal] = base
+    _concrete_iterator: Iter[Animal] = base
