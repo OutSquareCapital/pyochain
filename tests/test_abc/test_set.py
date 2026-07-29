6ee54b3ad7e45e3fdebd7b671ed9f1cf3fff1375
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-07-29 23:57:47 +0200

    refactor: fused all "homemade" sets tests in one file

--- tests/test_abc/test_set.py
+++ tests/test_abc/test_set.py
@@ -1,12 +1,19 @@
+from collections.abc import Callable, Iterable, Iterator, MutableSet
 from collections.abc import Set as AbstractSet
+from typing import override
 
 import pytest
 
-from pyochain.abc import PyoSet
+from pyochain import Dict, Seq, Set, SetMut
+from pyochain.abc import PyoMutableSet, PyoSet
 from tests.test_abc._utils import ImplContainer, ImplSized
 
 from . import checks
 from ._utils import ImplCollection, ImplIter, assert_iter_eq
+
+type SetPair[T] = tuple[AbstractSet[T], AbstractSet[T]]
+
+type IntoSetMutFn = Callable[[set[int]], MutableSet[int]]
 
 
 class _PyFail(AbstractSet[int]): ...  # pyright: ignore[reportImplicitAbstractClass]
@@ -58,3 +65,87 @@ def test_set_from_iterable_note() -> None:
         _ = a | b
     assert exc_info.value.__notes__
     assert "PyoSet" in exc_info.value.__notes__[0]
+
+
+class ConcreteSet(AbstractSet[int]):
+    def __init__(self, data: Iterable[int]) -> None:
+        self.data: set[int] = set(data)
+
+    @override
+    def __repr__(self) -> str:
+        return f"ConcreteSet({self.data})"
+
+    @override
+    def __contains__(self, item: object) -> bool:
+        return item in self.data
+
+    @override
+    def __iter__(self) -> Iterator[int]:
+        return iter(self.data)
+
+    @override
+    def __len__(self) -> int:
+        return len(self.data)
+
+
+class ConcretePyoSet(ConcreteSet, PyoSet[int]):
+    pass
+
+
+class ConcretePyoSetMut(ConcreteSet, PyoMutableSet[int]):
+    @override
+    def add(self, item: int) -> None:
+        self.data.add(item)
+
+    @override
+    def discard(self, item: int) -> None:
+        self.data.discard(item)
+
+
+def _get_all_sets() -> Dict[str, SetPair[int]]:
+    data = (1, 2, 3)
+    a = frozenset(data)
+    b = ConcreteSet(data)
+    c = set(data)
+    d = Set(data)
+    e = SetMut(data)
+    f = ConcretePyoSet(data)
+    return (
+        Seq((a, b, c, d, e, f))
+        .iter()
+        .combinations(2)
+        .map_star(
+            lambda x, y: (f"{x.__class__.__name__} and {y.__class__.__name__}", (x, y))
+        )
+        .collect(Dict[str, SetPair[int]])
+    )
+
+
+ALL_SETS = _get_all_sets()
+
+
+@pytest.mark.parametrize("sets", ALL_SETS.values(), ids=ALL_SETS.keys())
+def test_and_dunder(sets: SetPair[int]) -> None:
+    x, y = sets
+    assert x == y
+    assert x & y & y & x == x
+
+
+@pytest.mark.parametrize("x_type", (SetMut, set, ConcretePyoSetMut))
+@pytest.mark.parametrize("y_type", (SetMut, set, ConcretePyoSetMut))
+def test_iand_with_set(x_type: IntoSetMutFn, y_type: IntoSetMutFn) -> None:
+    base = {1, 2, 3}
+    x = x_type(base)
+    y = y_type({2, 3, 4})
+    x_id = id(x)
+    y_id = id(y)
+    y &= x
+    assert x == base
+    assert y == {2, 3}
+    assert id(x) == x_id
+    # set &= AbstractSet will return NotImplemented and then call AbstractSet.__rand__ which returns a new set, so the id will change.
+    # This means that set &= SetMut won't be in-place (contrary to the opposite operation), UNLESS SetMut becomes itself a subclass of set.
+    if (x_type is SetMut or x_type is ConcretePyoSetMut) and y_type is set:
+        return
+
+    assert id(y) == y_id
