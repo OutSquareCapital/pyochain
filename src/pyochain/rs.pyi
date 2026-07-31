39875828ad4fee3d45f16c919e617e9f4b57b72b
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-07-31 02:58:56 +0200

    chore: documented `rs.pyi`

--- src/pyochain/rs.pyi
+++ src/pyochain/rs.pyi
@@ -1,3 +1,16 @@
+"""This module is the actual "live" compiled module.
+
+However, we mirror the Rust code layout in the stubs with multiple "false" private `.pyi` files.
+
+Those are then re-exported in this module.
+
+That way, we can :
+
+- import from rs in the public __init__
+- Support `from pyochain import foo` at runtime
+- Handle partially migrated modules (python -> rust) without breaking the public API
+"""
+
 from collections.abc import Callable, Iterable
 from types import NotImplementedType
 from typing import Any, Self, overload, type_check_only
@@ -7,12 +20,6 @@ from _typeshed import SupportsRichComparison
 from pyochain.abc import PyoIterator
 
 from ._collections import Deque, Heap, HeapMax, HeapMin, PyoCounter, StableSet
-
-# Export for typing purposes. the private modules actually don't exist at runtime.
-# That way, we can :
-# - import from rs in the public __init__
-# - Support from pyochain import foo fully typed
-# - Handle partially migrated modules (python -> rust) without breaking the public API
 from ._dict import Dict
 from ._iterators import Iter, Peekable
 from ._option import NONE, Null, Option, OptionUnwrapError, Some, option
