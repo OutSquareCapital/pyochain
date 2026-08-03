440f1705acdf647d30693cb8ec32c1f2a55d4779
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-08-03 11:01:26 +0200

    fix: missed one pyclass

--- rust/src/collections/sorted/keylist.rs
+++ rust/src/collections/sorted/keylist.rs
@@ -10,7 +10,7 @@ use pyo3::{prelude::*, types::PyList};
 use std::ops::Index;
 use std::sync::{Mutex, atomic::AtomicUsize};
 use tap::Pipe;
-#[pyclass(frozen, generic)]
+#[pyclass(module = "pyochain.collections", frozen, generic)]
 pub struct InnerKeyLists {
     #[pyo3(get)]
     pub(super) key: Py<PyAny>,
