c10fd40e198441eddda16ad2847f1776bb024b53
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-08-05 12:49:16 +0200

    fix: incorrect ordering logic

--- rust/src/collections/sorted/cmp.rs
+++ rust/src/collections/sorted/cmp.rs
@@ -5,16 +5,16 @@ use std::cmp::Ordering;
 const MSG: &str = "Unexpected error during comparison";
 #[inline(always)]
 pub fn py_cmp(py: Python<'_>, a: &Py<PyAny>, b: &Py<PyAny>) -> Ordering {
-    a.bind(py).lt(b.bind(py)).map(gt_if_true).expect(MSG)
+    a.bind(py).lt(b.bind(py)).map(lt_if_true).expect(MSG)
 }
 #[inline(always)]
 pub fn py_cmp_by_key(a: &Py<PyAny>, b: &Py<PyAny>, key: &Bound<'_, PyAny>) -> Ordering {
     key.call1((a,))
         .and_then(|key_a| key_a.lt(key.call1((b,))?))
-        .map(gt_if_true)
+        .map(lt_if_true)
         .expect(MSG)
 }
 #[inline(always)]
-fn gt_if_true(b: bool) -> Ordering {
-    if b { Ordering::Greater } else { Ordering::Less }
+fn lt_if_true(b: bool) -> Ordering {
+    if b { Ordering::Less } else { Ordering::Greater }
 }
