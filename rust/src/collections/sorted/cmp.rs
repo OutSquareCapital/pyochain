eb01f3fb0e2dead54674a72f9809f97f33e7b186
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-08-05 11:56:21 +0200

    refactor: use MSG const for expect errors

--- rust/src/collections/sorted/cmp.rs
+++ rust/src/collections/sorted/cmp.rs
@@ -5,17 +5,14 @@ use std::cmp::Ordering;
 const MSG: &str = "Unexpected error during comparison";
 #[inline(always)]
 pub fn py_cmp(py: Python<'_>, a: &Py<PyAny>, b: &Py<PyAny>) -> Ordering {
-    a.bind(py)
-        .lt(b.bind(py))
-        .map(gt_if_true)
-        .expect("Unexpected error during comparison")
+    a.bind(py).lt(b.bind(py)).map(gt_if_true).expect(MSG)
 }
 #[inline(always)]
 pub fn py_cmp_by_key(a: &Py<PyAny>, b: &Py<PyAny>, key: &Bound<'_, PyAny>) -> Ordering {
     key.call1((a,))
         .and_then(|key_a| key_a.lt(key.call1((b,))?))
         .map(gt_if_true)
-        .expect("Unexpected error during comparison")
+        .expect(MSG)
 }
 #[inline(always)]
 fn gt_if_true(b: bool) -> Ordering {
