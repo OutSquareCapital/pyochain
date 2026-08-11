e49cd8f5e5b8249cd48c91360199c7d2c917216b
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-08-12 01:33:01 +0200

    fix: turns out having an `__init__` as a "true" python file is mandatory

--- pyochain/__init__.py
+++ pyochain/__init__.py
@@ -1,22 +1,30 @@
 """pyochain - A functional programming library for Python."""
 
-from ._dict import Dict
-from ._iterators import Iter, Peekable
-from ._option import NONE, Null, Option, OptionType, OptionUnwrapError, Some, option
-from ._range import Range
-from ._result import (
+from .pyochain import (
+    NONE,
+    Dict,
     Err,
+    Iter,
+    Null,
     Ok,
+    Option,
+    OptionType,
+    OptionUnwrapError,
+    Peekable,
+    Range,
     Result,
     ResultType,
     ResultUnwrapError,
+    Seq,
+    Set,
+    SetMut,
+    SliceView,
+    Some,
+    Vec,
+    option,
     then_if_some,
     then_if_true,
 )
-from ._seq import Seq
-from ._set import Set, SetMut
-from ._sliceview import SliceView
-from ._vec import Vec
 
 __all__ = [
     "NONE",
