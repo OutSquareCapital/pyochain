0e314b619d1721e3ffc584ec3a627bd03bdb9002
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-08-13 03:55:18 +0200

    refactor: reorganize lib module

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,24 @@ use pyo3_ext::{
 fn pyochain(m: &Bound<'_, PyModule>) -> PyResult<()> {
     debug_backtrace();
     let py = m.py();
+    let abc_mod = PyModule::new(py, "abc")?;
+    let collections_mod = PyModule::new(py, "collections")?;
+    let sorted_mod = PyModule::new(py, "_sorted")?;
+    let sys_mods = py.import("sys")?.getattr("modules")?;
+    m.add_submodule(&abc_mod)?;
+    m.add_submodule(&collections_mod)?;
+    collections_mod.add_submodule(&sorted_mod)?;
+    populate_core(m, py)?;
+    populate_abc(&abc_mod)?;
+    populate_collections(&collections_mod)?;
+    populate_sorted(&sorted_mod)?;
+    sys_mods.set_item("pyochain", m)?;
+    sys_mods.set_item("pyochain.abc", abc_mod)?;
+    sys_mods.set_item("pyochain.collections", collections_mod)?;
+    sys_mods.set_item("pyochain.collections._sorted", sorted_mod)?;
+    register_all(py)
+}
+fn populate_core(m: &Bound<'_, PyModule>, py: Python<'_>) -> PyResult<()> {
     option::PyNull::init(py)?;
     m.add_class::<option::PyochainOption>()?;
     m.add_class::<option::PyochainOptionType>()?;
@@ -50,62 +68,53 @@ fn pyochain(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<dict::Dict>()?;
     m.add_class::<iterators::Iter>()?;
     m.add_class::<iterators::Peekable>()?;
-    m.add_class::<sliceview::SliceView>()?;
-
-    let abc_mod = PyModule::new(py, "abc")?;
-    abc_mod.add_class::<abc::Checkable>()?;
-    abc_mod.add_class::<abc::Fluent>()?;
-    abc_mod.add_class::<abc::PyoPipe>()?;
-    abc_mod.add_class::<abc::PyoTap>()?;
-    abc_mod.add_class::<abc::PyoIterable>()?;
-    abc_mod.add_class::<abc::PyoIterator>()?;
-    abc_mod.add_class::<abc::PyoContainer>()?;
-    abc_mod.add_class::<abc::PyoSized>()?;
-    abc_mod.add_class::<abc::PyoCollection>()?;
-    abc_mod.add_class::<abc::PyoReversible>()?;
-    abc_mod.add_class::<abc::PyoSequence>()?;
-    abc_mod.add_class::<abc::PyoMutableSequence>()?;
-    abc_mod.add_class::<abc::PyoSet>()?;
-    abc_mod.add_class::<abc::PyoMutableSet>()?;
-    abc_mod.add_class::<abc::PyoMappingView>()?;
-    abc_mod.add_class::<abc::PyoMapping>()?;
-    abc_mod.add_class::<abc::PyoKeysView>()?;
-    abc_mod.add_class::<abc::PyoValuesView>()?;
-    abc_mod.add_class::<abc::PyoItemsView>()?;
-    abc_mod.add_class::<abc::PyoMutableMapping>()?;
-    m.add_submodule(&abc_mod)?;
-    let collections_mod = PyModule::new(py, "collections")?;
-    collections_mod.add_class::<collections::StableSet>()?;
-    collections_mod.add_class::<collections::Deque>()?;
-    collections_mod.add_class::<collections::PyoCounter>()?;
-    collections_mod.add_class::<collections::Heap>()?;
-    collections_mod.add_class::<collections::HeapMax>()?;
-    collections_mod.add_class::<collections::HeapMin>()?;
-    collections_mod.add_class::<collections::SortedList>()?;
-    collections_mod.add_class::<collections::SortedKeyList>()?;
-    collections_mod.add_class::<collections::SortedSet>()?;
-    collections_mod.add_class::<collections::SortedKeySet>()?;
-    collections_mod.add_class::<collections::SortedDict>()?;
-    collections_mod.add_class::<collections::SortedKeyDict>()?;
-    collections_mod.add_class::<collections::sorted::SortedKeysView>()?;
-    collections_mod.add_class::<collections::sorted::SortedValuesView>()?;
-    collections_mod.add_class::<collections::sorted::SortedItemsView>()?;
-    m.add_submodule(&collections_mod)?;
-    let sorted_mod = PyModule::new(py, "_sorted")?;
-    sorted_mod.add_function(wrap_pyfunction!(debug::check_sorted_list, &sorted_mod)?)?;
-    sorted_mod.add_function(wrap_pyfunction!(debug::check_sorted_key_list, &sorted_mod)?)?;
-    sorted_mod.add_function(wrap_pyfunction!(
-        debug::assert_sorted_list_empty,
-        &sorted_mod
-    )?)?;
-    sorted_mod.add_function(wrap_pyfunction!(debug::check_sorted_set, &sorted_mod)?)?;
-    sorted_mod.add_function(wrap_pyfunction!(debug::check_sorted_dict, &sorted_mod)?)?;
-    let sys_mods = py.import("sys")?.getattr("modules")?;
-    sys_mods.set_item("pyochain", m)?;
-    sys_mods.set_item("pyochain.abc", abc_mod)?;
-    sys_mods.set_item("pyochain.collections", collections_mod)?;
-    sys_mods.set_item("pyochain.collections._sorted", sorted_mod)?;
-    register_all(py)
+    m.add_class::<sliceview::SliceView>()
+}
+fn populate_abc(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<abc::Checkable>()?;
+    m.add_class::<abc::Fluent>()?;
+    m.add_class::<abc::PyoPipe>()?;
+    m.add_class::<abc::PyoTap>()?;
+    m.add_class::<abc::PyoIterable>()?;
+    m.add_class::<abc::PyoIterator>()?;
+    m.add_class::<abc::PyoContainer>()?;
+    m.add_class::<abc::PyoSized>()?;
+    m.add_class::<abc::PyoCollection>()?;
+    m.add_class::<abc::PyoReversible>()?;
+    m.add_class::<abc::PyoSequence>()?;
+    m.add_class::<abc::PyoMutableSequence>()?;
+    m.add_class::<abc::PyoSet>()?;
+    m.add_class::<abc::PyoMutableSet>()?;
+    m.add_class::<abc::PyoMappingView>()?;
+    m.add_class::<abc::PyoMapping>()?;
+    m.add_class::<abc::PyoKeysView>()?;
+    m.add_class::<abc::PyoValuesView>()?;
+    m.add_class::<abc::PyoItemsView>()?;
+    m.add_class::<abc::PyoMutableMapping>()
+}
+fn populate_collections(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<collections::StableSet>()?;
+    m.add_class::<collections::Deque>()?;
+    m.add_class::<collections::PyoCounter>()?;
+    m.add_class::<collections::Heap>()?;
+    m.add_class::<collections::HeapMax>()?;
+    m.add_class::<collections::HeapMin>()?;
+    m.add_class::<collections::SortedList>()?;
+    m.add_class::<collections::SortedKeyList>()?;
+    m.add_class::<collections::SortedSet>()?;
+    m.add_class::<collections::SortedKeySet>()?;
+    m.add_class::<collections::SortedDict>()?;
+    m.add_class::<collections::SortedKeyDict>()?;
+    m.add_class::<collections::sorted::SortedKeysView>()?;
+    m.add_class::<collections::sorted::SortedValuesView>()?;
+    m.add_class::<collections::sorted::SortedItemsView>()
+}
+fn populate_sorted(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(debug::check_sorted_list, m)?)?;
+    m.add_function(wrap_pyfunction!(debug::check_sorted_key_list, m)?)?;
+    m.add_function(wrap_pyfunction!(debug::assert_sorted_list_empty, m)?)?;
+    m.add_function(wrap_pyfunction!(debug::check_sorted_set, m)?)?;
+    m.add_function(wrap_pyfunction!(debug::check_sorted_dict, m)?)
 }
 fn register_all(py: Python<'_>) -> PyResult<()> {
     let abc_mod = py.import("collections.abc")?;
