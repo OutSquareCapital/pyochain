3ecf243cb02b3830b74e6d6b4da6001ec2314498
OutSquareCapital <outsquarecapital@gmail.com>
2026-08-21 17:28:08 +0200

    refactor: forward error correctly to main

--- crates/pyochain_build/src/lib.rs
+++ crates/pyochain_build/src/lib.rs
@@ -1,13 +1,13 @@
 mod generate_docs;
 mod parse;
 mod paths;
+use std::error::Error;
 use std::path::PathBuf;
 
-pub fn run(root: PathBuf) {
+pub fn run(root: PathBuf) -> Result<(), Box<dyn Error>> {
     let stubs = paths::Related::new(root.join("pyochain"), "pyochain", "pyi");
     let docs = paths::Related::new(root.join("docs"), "reference", "md");
     let src = paths::Related::new(root.join("src"), "lib.rs", "rs");
     let zensical = paths::Related::new(root, "zensical.toml", "toml");
     generate_docs::run(zensical, src, docs, stubs)
-        .expect("Failed to generate documentation due to an unexpected error.");
 }
--- crates/pyochain_build/src/main.rs
+++ crates/pyochain_build/src/main.rs
@@ -1,10 +1,15 @@
 use anstream::ColorChoice;
-use std::path::PathBuf;
+use std::{path::PathBuf, process::ExitCode};
 use tap::Pipe;
 
-fn main() {
+fn main() -> ExitCode {
     ColorChoice::Always.write_global();
     PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .join("../..")
         .pipe(pyochain_build::run)
+        .map(|_| ExitCode::SUCCESS)
+        .unwrap_or_else(|error| {
+            anstream::eprintln!("Documentation generation failed: {error}");
+            ExitCode::FAILURE
+        })
 }
