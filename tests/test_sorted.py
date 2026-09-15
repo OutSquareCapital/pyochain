5499366faba81843d56e68d56030904a431dfab0
OutSquareCapital <outsquarecapital@gmail.com>
2026-09-15 16:57:59 +0200

    feat: added bisect tests

--- tests/test_sorted.py
+++ tests/test_sorted.py
@@ -1,14 +1,17 @@
 from __future__ import annotations
 
+from operator import neg
 from typing import TYPE_CHECKING
 
 import pytest
 
 from pyochain import Range
-from pyochain.collections import SortedSet
+from pyochain.collections import SortedKeySet, SortedSet
 
 if TYPE_CHECKING:
     from collections.abc import Callable
+
+    from pyochain.collections._sorted import BaseSortedSet
 
 
 @pytest.mark.parametrize(
@@ -20,3 +23,9 @@ def test_update_set_deadlock(
     data = Range(3)
     a = SortedSet(data)
     assert fn(a, a) == a
+
+
+@pytest.mark.parametrize("data", (SortedSet(range(5)), SortedKeySet(neg, range(5))))
+def test_set_bisect(data: BaseSortedSet[int]) -> None:
+    assert data.bisect_left(2) == 2
+    assert data.bisect_right(2) == 3
