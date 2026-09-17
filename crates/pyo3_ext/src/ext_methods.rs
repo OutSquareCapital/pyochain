e9af5e2635d65a64bb55874314ad70e32496b546
OutSquareCapital <outsquarecapital@gmail.com>
2026-09-17 22:56:05 +0200

    refactor: backtracked as well the `1` methods versions, as they were mostly bloat

--- crates/pyo3_ext/src/ext_methods.rs
+++ crates/pyo3_ext/src/ext_methods.rs
@@ -122,15 +122,7 @@ pub trait PySetExtMethods<'py>: Sized {
 }
 pub trait PySetExtMethodsMut<'py>: PySetExtMethods<'py> {
     fn difference_update<O: PyCallArgs<'py>>(&self, s: O) -> PyResult<()>;
-    #[inline]
-    fn difference_update1<T: IntoPyObject<'py>>(&self, s: T) -> PyResult<()> {
-        self.difference_update((s,))
-    }
     fn intersection_update<O: PyCallArgs<'py>>(&self, s: O) -> PyResult<()>;
-    #[inline]
-    fn intersection_update1<T: IntoPyObject<'py>>(&self, s: T) -> PyResult<()> {
-        self.intersection_update((s,))
-    }
     fn remove<T: IntoPyObject<'py>>(&self, element: T) -> PyResult<()>;
     fn symmetric_difference_update<T: IntoPyObject<'py>>(&self, s: T) -> PyResult<()>;
     fn update<O: PyCallArgs<'py>>(&self, s: O) -> PyResult<()>;
