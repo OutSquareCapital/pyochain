59f4e530ecea1da90b915ee4a3181922501ece85
OutSquareCapital <outsquarecapital@gmail.com>
2026-09-17 17:03:42 +0200

    feat: added new tests for deadlocking

--- tests/test_sorted.py
+++ tests/test_sorted.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections.abc import Iterable, Sequence
 from operator import neg
 from typing import TYPE_CHECKING
 
@@ -12,20 +13,41 @@ if TYPE_CHECKING:
     from collections.abc import Callable
 
     from pyochain.collections._sorted import BaseSortedSet
+type UpdateFn = Callable[[SortedSet[int], *tuple[Iterable[int]]], None]
+DATA = Range(5)
+DATA_SET = SortedSet(DATA)
 
-
-@pytest.mark.parametrize(
-    "fn", (SortedSet[int].update, SortedSet[int].difference_update)
+VALUES_PARAMS = pytest.mark.parametrize(
+    "data",
+    ([DATA], [DATA, DATA], [DATA_SET], [DATA_SET, DATA_SET]),
 )
-def test_update_set_deadlock(
-    fn: Callable[[SortedSet[int], SortedSet[int]], None],
-) -> None:
-    data = Range(3)
-    a = SortedSet(data)
-    assert fn(a, a) == a
 
 
-@pytest.mark.parametrize("data", (SortedSet(range(5)), SortedKeySet(neg, range(5))))
+@VALUES_PARAMS
+@pytest.mark.parametrize(
+    "fn", (SortedSet[int].update, SortedSet[int].intersection_update)
+)
+def test_update_deadlock(data: Iterable[Iterable[int]], fn: UpdateFn) -> None:
+    a = SortedSet(DATA)
+    fn(a, *data)
+    assert a == DATA_SET
+
+
+@VALUES_PARAMS
+def test_diff_update_deadlock(data: Sequence[Iterable[int]]) -> None:
+    a = SortedSet(DATA)
+    a.difference_update(*data)
+    assert a.is_empty()
+
+
+@pytest.mark.parametrize("data", (DATA, DATA_SET))
+def test_symmetric_diff_update_deadlock(data: Iterable[int]) -> None:
+    a = SortedSet(DATA)
+    a.symmetric_difference_update(data)
+    assert a.is_empty()
+
+
+@pytest.mark.parametrize("data", (DATA_SET, SortedKeySet(neg, DATA)))
 def test_set_bisect(data: BaseSortedSet[int]) -> None:
     assert data.bisect_left(2) == 2
     assert data.bisect_right(2) == 3
