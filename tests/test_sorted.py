ab0dd3974eaf6d58cefc43eeb4d106b5cdd5316a
OutSquareCapital <outsquarecapital@gmail.com>
2026-09-22 21:03:07 +0200

    feat: add tests for empty args in set ops

--- tests/test_sorted.py
+++ tests/test_sorted.py
@@ -16,8 +16,9 @@ if TYPE_CHECKING:
     from _pytest.mark import ParameterSet
 
     from pyochain.collections._sorted import BaseSortedSet
-type UpdateFn = Callable[[SortedSet[int], *tuple[Iterable[int]]], None]
 type UpdateFn1[T] = Callable[[SortedSet[int], Iterable[int]], T]
+type PyUpdateFn1[*A, T] = Callable[[set[int], *A], T]
+type UpdateVarFn[*A, T] = Callable[[SortedSet[int], *A], T]
 type SortedSetFactory = Callable[[], BaseSortedSet[int]]
 DATA = Range(5)
 PY_EMPTY = set[int]()
@@ -34,6 +35,10 @@ def test_set_bisect(data: BaseSortedSet[int]) -> None:
 
 def _param[T](fn: UpdateFn1[T], expected: T) -> ParameterSet:
     return pytest.param(fn, expected, id=fn.__name__)
+
+
+def _param1[T](fn: UpdateFn1[T], pyfn: PyUpdateFn1[T]) -> ParameterSet:
+    return pytest.param(fn, pyfn, id=fn.__name__)
 
 
 @pytest.mark.parametrize(
@@ -76,3 +81,36 @@ def test_deadlock_mut[T](method: UpdateFn1[object], expected: object) -> None:
     a = SortedSet(DATA)
     _ = method(a, a)
     assert a == expected
+
+
+@pytest.mark.parametrize(
+    ("method", "pymethod"),
+    (
+        _param1(SortedSet[int].difference, set[int].difference),
+        _param1(SortedSet[int].union, set[int].union),
+        _param1(SortedSet[int].intersection, set[int].intersection),
+    ),
+)
+def test_empty(
+    method: UpdateVarFn[AbstractSet[int]], pymethod: PyUpdateFn1[AbstractSet[int]]
+) -> None:
+    a = SortedSet(DATA)
+    assert set(method(a)) == pymethod(PY_DATA_SET.copy())
+
+
+@pytest.mark.parametrize(
+    ("method", "pymethod"),
+    (
+        _param1(SortedSet[int].difference_update, set[int].difference_update),
+        _param1(SortedSet[int].update, set[int].update),
+        _param1(SortedSet[int].intersection_update, set[int].intersection_update),
+    ),
+)
+def test_empty_mut[T](
+    method: UpdateVarFn[object], pymethod: PyUpdateFn1[object]
+) -> None:
+    a = SortedSet(DATA)
+    b = PY_DATA_SET.copy()
+    _ = method(a)
+    _ = pymethod(b)
+    assert a == b
