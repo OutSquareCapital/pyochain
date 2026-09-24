3a4c1683d937c57e954bb685b59132ed4b054c04
OutSquareCapital <outsquarecapital@gmail.com>
2026-09-24 14:17:20 +0200

    refactor: rename RwLockExt methods

--- crates/std_tools/src/sync.rs
+++ crates/std_tools/src/sync.rs
@@ -29,22 +29,27 @@ impl<T> MutexExtMethods<T> for Mutex<T> {
     }
 }
 pub trait RwLockExtMethods<T> {
-    fn try_into_inner(&self) -> RwLockReadGuard<'_, T>;
-    fn write_or_inner(&self) -> RwLockWriteGuard<'_, T>;
-}
-
-impl<T> RwLockExtMethods<T> for RwLock<T> {
     /// Read the inner value of the `RwLock`.\
     /// If the lock is poisoned, it will return the inner value of said lock.
-    #[inline(always)]
-    fn try_into_inner(&self) -> RwLockReadGuard<'_, T> {
-        std::sync::RwLock::read(self).unwrap_or_else(PoisonError::into_inner)
-    }
+    fn read_or_inner(&self) -> RwLockReadGuard<'_, T>;
+    fn write_or_inner(&self) -> RwLockWriteGuard<'_, T>;
     /// Tries to acquire the write lock and returns the inner value.\
     /// If the lock is poisoned, it will return the inner value of the poisoned lock.
     /// If the lock is already held (by this or another thread), it will panic.
+    fn try_write_or_inner(&self) -> RwLockWriteGuard<'_, T>;
+}
+
+impl<T> RwLockExtMethods<T> for RwLock<T> {
+    #[inline(always)]
+    fn read_or_inner(&self) -> RwLockReadGuard<'_, T> {
+        std::sync::RwLock::read(self).unwrap_or_else(PoisonError::into_inner)
+    }
     #[inline(always)]
     fn write_or_inner(&self) -> RwLockWriteGuard<'_, T> {
+        std::sync::RwLock::write(self).unwrap_or_else(PoisonError::into_inner)
+    }
+    #[inline(always)]
+    fn try_write_or_inner(&self) -> RwLockWriteGuard<'_, T> {
         match self.try_write() {
             Ok(guard) => guard,
             Err(TryLockError::Poisoned(poisoned)) => poisoned.into_inner(),
